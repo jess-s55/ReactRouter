38152b295fbd4e9ee4838ad0126cd10dd9bdd9ed
Jessica Spivak <jls103193@yahoo.com>
2023-07-31 08:59:40 -0400

    NavBar tab complete

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ function App() {
         </div>
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={ Blue } />
-            <Route path="/red" element={ Red } />
-            <Route path="/" element={ Home } />
+            <Route path="/blue" element={ <Blue /> } />
+            <Route path="/red" element={ <Red /> } />
+            <Route path="/" element={ <Home /> } />
           </Routes>
         </div>
       </div>
