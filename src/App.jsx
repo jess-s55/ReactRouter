564b886701a44f1332b77710ad307e10d806e2b6
Jessica Spivak <jls103193@yahoo.com>
2023-07-31 13:18:20 -0400

    styling added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import  Blue from './components/blue.jsx';
 import  Red from './components/red.jsx';
 import  Home from './components/home.jsx'
+import Green from './components/green.jsx'
 
 function App() {
   
@@ -12,15 +13,17 @@ function App() {
       <div id="container">
         
         <div id="navbar">
-          <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link>
-          <Link to="/">Home</Link>
+          <Link className="homeLink" to="/">Home</Link>
+          <Link className="blueLink" to="/blue">Blue</Link>
+          <Link className="redLink" to="/red">Red</Link>
+          <Link className="greenLink" to="/green">Green</Link>
         </div>
         <div id="main-section">
           <Routes>
+            <Route path="/" element={ <Home /> } />
             <Route path="/blue" element={ <Blue /> } />
             <Route path="/red" element={ <Red /> } />
-            <Route path="/" element={ <Home /> } />
+            <Route path="/green" element={ <Green /> } />
           </Routes>
         </div>
       </div>
